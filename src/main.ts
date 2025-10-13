00a528de02555723c9786d293c96fddba701165b
behzad 1k <bhzd1k@gmail.com>
2025-10-13 11:35:56 +0330

    auth route in email

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   // app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
