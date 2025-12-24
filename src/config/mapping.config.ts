7a5b9650485394a4c33e891cb37fccf0722da6aa
behzad 1k <bhzd1k@gmail.com>
2025-12-24 06:26:08 +0330

    fixes

--- src/config/mapping.config.ts
+++ src/config/mapping.config.ts
@@ -1,100 +1,126 @@
 export const EXTERNAL_MAPPINGS = {
-  lastFM: {
-    track: {
-      title: 'name',
-      externalListens: 'listeners',
-      lastFMLink: 'url',
-      artistName: (data: any) => data.artist ? typeof data.artist == 'string' ? data.artist : data.artist.name : '',
-      albumName: 'albumName',
-      albumCover: 'albumCover',
-      mbid: 'mbid',
-      duration: 'duration',
-    },
-    artist: {
-      name: 'name',
-      mbid: 'mbid',
-      lastFMLink: 'url',
-      pfp: 'pfp',
-      externalListeners: (data: any)=> data.stats ? data.stats?.listeners : data.listeners,
-      externalPlays: (data: any)=> data.stats?.playcount,
-      bio: (data: any)=> data.bio?.summary,
-      fullBio: (data: any)=> data.bio?.content,
-    },
-    album: {
-      title: 'name',
-      mbid: 'mbid',
-      lastFMLink: 'url',
-      externalListens: 'listeners',
-      externalPlays: 'playcount',
-      tracks: (data: any) => data.tracks,
-      artistName: (data: any)=> data.artist?.name || data.artist,
-      albumCover: (data: any) => data.image ? (Array.isArray(data.image) ? data.image[data.image.length -1]['#text'] : data.image['#text']) : '',
-      releaseDate: (data: any)=> data.wiki ? data.wiki?.published : null,
-      description: (data: any) => data.wiki? data?.wiki?.content : null
-    }
-  },
-  discogs: {
-    track: {
-      title: 'title',
-      artist: (data: any) => data.artists?.[0]?.name,
-      albumCover: (data: any) => data.cover_image || data.thumb,
-    }
-  },
-  amazon: {
-    track: {
-      title: 'title',
-      asin: 'asin',
-      duration: 'duration',
-      externalId: 'id',
-      amazonUrl: 'url',
-      albumCover: 'image',
-      artistName: (data: any) => data.artist ? typeof data.artist == 'string' ? data.artist : data.artist?.name : '',
-      releaseData: 'original_release_date',
-      albumName: (data: any) => data.album ? typeof data.album == 'string' ? data.album : data.album.name : '',
-      genre: 'genre',
-      isExplicit: 'explicit',
-    },
-    artist: {
-      name: 'name',
-      asin: 'asin',
-      amazonUrl: 'url',
-      externalId: 'id',
-      pfp: 'image',
-      externalListeners: (data: any)=> data.stats ? data.stats?.listeners : data.listeners,
-      externalPlays: (data: any)=> data.stats?.playcount,
-      bio: (data: any)=> data.bio?.summary,
-      fullBio: (data: any)=> data.bio?.content,
-    },
-    album: {
-      title: (data: any) => data.name || data.title,
-      artistName: (data: any) => data.artist ? typeof data.artist == 'string' ? data.artist : data.artist.name : '',
-      amazonUrl: 'url',
-      asin: 'asin',
-      externalId: 'id',
-      releaseData: 'release_date',
-      isExplicit: 'explicit',
-      track_count: 'track_count',
-      genre: 'genre',
-      // externalListens: 'listeners',
-      // externalPlays: 'playcount',
-      albumCover: 'image',
-      // rankForArtist: (data: any)=> data['@attr']?.rank,
-    }
-  }
+	lastFM: {
+		track: {
+			title: "name",
+			externalListens: "listeners",
+			lastFMLink: "url",
+			artistName: (data: any) =>
+				data.artist
+					? typeof data.artist == "string"
+						? data.artist
+						: data.artist?.name
+					: "",
+			albumName: "albumName",
+			albumCover: "albumCover",
+			mbid: "mbid",
+			duration: "duration",
+		},
+		artist: {
+			name: "name",
+			mbid: "mbid",
+			lastFMLink: "url",
+			pfp: "pfp",
+			externalListeners: (data: any) =>
+				data.stats ? data.stats?.listeners : data.listeners,
+			externalPlays: (data: any) => data.stats?.playcount,
+			bio: (data: any) => data.bio?.summary,
+			fullBio: (data: any) => data.bio?.content,
+		},
+		album: {
+			title: "name",
+			mbid: "mbid",
+			lastFMLink: "url",
+			externalListens: "listeners",
+			externalPlays: "playcount",
+			tracks: (data: any) => data.tracks,
+			artistName: (data: any) => data.artist?.name || data.artist,
+			albumCover: (data: any) =>
+				data.image
+					? Array.isArray(data.image)
+						? data.image[data.image.length - 1]["#text"]
+						: data.image["#text"]
+					: "",
+			releaseDate: (data: any) => (data.wiki ? data.wiki?.published : null),
+			description: (data: any) => (data.wiki ? data?.wiki?.content : null),
+		},
+	},
+	discogs: {
+		track: {
+			title: "title",
+			artist: (data: any) => data.artists?.[0]?.name,
+			albumCover: (data: any) => data.cover_image || data.thumb,
+		},
+	},
+	amazon: {
+		track: {
+			title: "title",
+			asin: "asin",
+			duration: "duration",
+			externalId: "id",
+			amazonUrl: "url",
+			albumCover: "image",
+			artistName: (data: any) =>
+				data.artist
+					? typeof data.artist == "string"
+						? data.artist
+						: data.artist?.name
+					: "",
+			releaseData: "original_release_date",
+			albumName: (data: any) =>
+				data.album
+					? typeof data.album == "string"
+						? data.album
+						: data.album.name
+					: "",
+			genre: "genre",
+			isExplicit: "explicit",
+		},
+		artist: {
+			name: "name",
+			asin: "asin",
+			amazonUrl: "url",
+			externalId: "id",
+			pfp: "image",
+			externalListeners: (data: any) =>
+				data.stats ? data.stats?.listeners : data.listeners,
+			externalPlays: (data: any) => data.stats?.playcount,
+			bio: (data: any) => data.bio?.summary,
+			fullBio: (data: any) => data.bio?.content,
+		},
+		album: {
+			title: (data: any) => data.name || data.title,
+			artistName: (data: any) =>
+				data.artist
+					? typeof data.artist == "string"
+						? data.artist
+						: data.artist.name
+					: "",
+			amazonUrl: "url",
+			asin: "asin",
+			externalId: "id",
+			releaseData: "release_date",
+			isExplicit: "explicit",
+			track_count: "track_count",
+			genre: "genre",
+			// externalListens: 'listeners',
+			// externalPlays: 'playcount',
+			albumCover: "image",
+			// rankForArtist: (data: any)=> data['@attr']?.rank,
+		},
+	},
 };
-
 
 // Utility to apply mappings
 export function applyMapping<T>(source: any, mappingConfig: any): T {
-  const result: any = {};
+	const result: any = {};
 
-  for (const [targetKey, mapping] of Object.entries(mappingConfig)) {
-    if (typeof mapping === 'function') {
-      result[targetKey] = mapping(source);
-    } else if (typeof mapping === 'string') {
-      result[targetKey] = source[mapping];
-    }
-  }
+	for (const [targetKey, mapping] of Object.entries(mappingConfig)) {
+		if (typeof mapping === "function") {
+			result[targetKey] = mapping(source);
+		} else if (typeof mapping === "string") {
+			result[targetKey] = source[mapping];
+		}
+	}
 
-  return result as T;
+	return result as T;
 }
--- src/modules/music/lastfm.service.ts
+++ src/modules/music/lastfm.service.ts
@@ -1,197 +1,243 @@
-import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import { applyMapping, EXTERNAL_MAPPINGS } from '../../config/mapping.config';
-import { SearchFilter } from '../../types';
-import { SEARCH_FILTERS } from '../../utils/enums';
-import { Album } from './entities/album.entity';
-import { Artist } from './entities/artist.entity';
-import { Song } from './entities/song.entity';
+import { Injectable } from "@nestjs/common";
+import axios from "axios";
+import { applyMapping, EXTERNAL_MAPPINGS } from "../../config/mapping.config";
+import { SearchFilter } from "../../types";
+import { SEARCH_FILTERS } from "../../utils/enums";
+import { Album } from "./entities/album.entity";
+import { Artist } from "./entities/artist.entity";
+import { Song } from "./entities/song.entity";
 
 @Injectable()
 export class LastfmService {
-  private lastFmApiKey = process.env.LASTFM_API_KEY;
+	private lastFmApiKey = process.env.LASTFM_API_KEY;
 
-  async formatResult(result: any, filter: SEARCH_FILTERS, key: string = 'albumCover'): Promise<any> {
-    const finalResult = []
+	async formatResult(
+		result: any,
+		filter: SEARCH_FILTERS,
+		key: string = "albumCover",
+	): Promise<any> {
+		const finalResult = [];
 
-    try {
-      const fetchResult = await Promise.all(result.map(obj =>
-        axios.get(obj.url, {
-          timeout: 100000,
-          maxRedirects: 3,
-          headers: { 'User-Agent': 'Mozilla/5.0' } // Some sites require this
-        })
-      ));
-      fetchResult.map((data, index) => {
-        const coverRegex = /<meta[^>]*(?:property|name)=["'](og:)(image)["'][^>]*content=["']([^"']+)["'][^>]*>/gi.exec(data.data);
-        if (filter == SEARCH_FILTERS.track) {
-          const albumNameRegex = (/<a[^>]*class="link-block-target"[^>]*>([^<]+)<\/a>/gi).exec(data.data);
-          const durationRegex = (/<dd[^>]*class="catalogue-metadata-description"[^>]*>([^<]+)<\/dd>/gi).exec(data.data);
-          if (albumNameRegex && albumNameRegex[1]) result[index]['albumName'] = albumNameRegex[1];
-          if (durationRegex && durationRegex[1]) {
-            const duration = durationRegex[1].trim().split(':');
-            let finalDuration = parseInt(duration[duration.length - 2].substring(duration[duration.length - 2].length - 2)) * 60 + parseInt(duration[duration.length - 1]);
-            if (duration.length > 2) {
-              finalDuration = finalDuration + (parseInt(duration[0]) * 3600);
-            }
-            result[index]['duration'] = finalDuration;
-          }
-        } else if (filter == SEARCH_FILTERS.artist){
-          const listenersRegex = (/<abbr[^>]*\stitle="([^"]*)"[^>]*>/gi).exec(data.data)
-          if (listenersRegex && listenersRegex[1]) result[index]['listeners'] = parseInt(listenersRegex[1].replaceAll(',', ''));
-        }
-        if (coverRegex && coverRegex[3]) result[index][key] = coverRegex[3];
-        finalResult.push(applyMapping(result[index], EXTERNAL_MAPPINGS.lastFM[filter]))
-      });
-    } catch (e) {
-      console.log(e?.message);
-      console.log(e?.response?.data);
-    }
-    return finalResult;
-  }
+		try {
+			const fetchResult = await Promise.all(
+				result.map((obj) =>
+					axios.get(obj.url, {
+						timeout: 100000,
+						maxRedirects: 3,
+						headers: { "User-Agent": "Mozilla/5.0" }, // Some sites require this
+					}),
+				),
+			);
+			fetchResult.map((data, index) => {
+				const coverRegex =
+					/<meta[^>]*(?:property|name)=["'](og:)(image)["'][^>]*content=["']([^"']+)["'][^>]*>/gi.exec(
+						data.data,
+					);
+				if (filter == SEARCH_FILTERS.track) {
+					const albumNameRegex =
+						/<a[^>]*class="link-block-target"[^>]*>([^<]+)<\/a>/gi.exec(
+							data.data,
+						);
+					const durationRegex =
+						/<dd[^>]*class="catalogue-metadata-description"[^>]*>([^<]+)<\/dd>/gi.exec(
+							data.data,
+						);
+					if (albumNameRegex && albumNameRegex[1])
+						result[index]["albumName"] = albumNameRegex[1];
+					if (durationRegex && durationRegex[1]) {
+						const duration = durationRegex[1].trim().split(":");
+						let finalDuration =
+							parseInt(
+								duration[duration.length - 2].substring(
+									duration[duration.length - 2].length - 2,
+								),
+							) *
+								60 +
+							parseInt(duration[duration.length - 1]);
+						if (duration.length > 2) {
+							finalDuration = finalDuration + parseInt(duration[0]) * 3600;
+						}
+						result[index]["duration"] = finalDuration;
+					}
+				} else if (filter == SEARCH_FILTERS.artist) {
+					const listenersRegex = /<abbr[^>]*\stitle="([^"]*)"[^>]*>/gi.exec(
+						data.data,
+					);
+					if (listenersRegex && listenersRegex[1])
+						result[index]["listeners"] = parseInt(
+							listenersRegex[1].replaceAll(",", ""),
+						);
+				}
+				if (coverRegex && coverRegex[3]) result[index][key] = coverRegex[3];
+				finalResult.push(
+					applyMapping(result[index], EXTERNAL_MAPPINGS.lastFM[filter]),
+				);
+			});
+		} catch (e) {
+			console.log(e?.message);
+			console.log(e?.response?.data);
+		}
+		return finalResult;
+	}
 
-  async trackSearch(query: string, limit = 20): Promise<LastFM.Track[]> {
-    const result = await this.search(query, SEARCH_FILTERS.track, limit);
-    return result.trackmatches.track;
-  }
+	async trackSearch(query: string, limit = 20): Promise<LastFM.Track[]> {
+		const result = await this.search(query, SEARCH_FILTERS.track, limit);
+		return result.trackmatches.track;
+	}
 
-  async albumSearch(query: string, limit = 20): Promise<LastFM.Album[]> {
-    const result = await this.search(query, SEARCH_FILTERS.album, limit);
-    return result.albummatches.album;
-  }
+	async albumSearch(query: string, limit = 20): Promise<LastFM.Album[]> {
+		const result = await this.search(query, SEARCH_FILTERS.album, limit);
+		return result.albummatches.album;
+	}
 
-  async artistSearch(query: string, limit = 20): Promise<LastFM.Artist[]> {
-    const result = await this.search(query, SEARCH_FILTERS.artist, limit);
-    return result.artistmatches.artist;
-  }
+	async artistSearch(query: string, limit = 20): Promise<LastFM.Artist[]> {
+		const result = await this.search(query, SEARCH_FILTERS.artist, limit);
+		return result.artistmatches.artist;
+	}
 
-  private async search(query: string, filter: SearchFilter, limit: number) {
-    try {
-      const params = {
-        method: `${filter}.search`,
-        api_key: this.lastFmApiKey,
-        format: 'json',
-        limit
-      };
-      params[filter] = query;
-      const response = await axios.get('http://ws.audioscrobbler.com/2.0/', {
-        params
-      });
+	private async search(query: string, filter: SearchFilter, limit: number) {
+		try {
+			const params = {
+				method: `${filter}.search`,
+				api_key: this.lastFmApiKey,
+				format: "json",
+				limit,
+			};
+			params[filter] = query;
+			const response = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+				params,
+			});
 
-      return response.data.results || [];
-    } catch (error) {
-      return [];
-    }
-  }
+			return response.data.results || [];
+		} catch (error) {
+			return [];
+		}
+	}
 
-  async getArtistData(artist: Artist): Promise<Artist> {
-    const params: any = {
-      method: 'artist.getinfo',
-      api_key: this.lastFmApiKey,
-      format: 'json',
-      artist: artist.name,
-    };
-    if (artist.mbid) params.mbid = artist.mbid;
-    const response = await axios.get('http://ws.audioscrobbler.com/2.0/', { params });
-    return response.data.artist;
-  }
+	async getArtistData(artist: Artist): Promise<Artist> {
+		const params: any = {
+			method: "artist.getinfo",
+			api_key: this.lastFmApiKey,
+			format: "json",
+			artist: artist.name,
+		};
+		if (artist.mbid) params.mbid = artist.mbid;
+		const response = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+			params,
+		});
+		return response.data.artist;
+	}
 
-  async getSimilarTracks(song: Song, limit = 5): Promise<Song[]> {
-    const params: any = {
-      method: 'track.getsimilar',
-      api_key: this.lastFmApiKey,
-      format: 'json',
-      track: song.title,
-      artist: song.artistName,
-      limit
-    };
-    if (song.mbid) {
-      params.mbid = song.mbid;
-    }
-    const response = await axios.get('http://ws.audioscrobbler.com/2.0/', { params });
+	async getSimilarTracks(song: Song, limit = 5): Promise<Song[]> {
+		const params: any = {
+			method: "track.getsimilar",
+			api_key: this.lastFmApiKey,
+			format: "json",
+			track: song.title,
+			artist: song.artistName,
+			limit,
+		};
+		if (song.mbid) {
+			params.mbid = song.mbid;
+		}
+		const response = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+			params,
+		});
 
-    return response.data.similartracks.track;
-  }
+		return response.data.similartracks.track;
+	}
 
-  async getArtistTopSongs(artist: Artist, limit = 10): Promise<LastFM.Track[]> {
-    const params: any = {
-      method: 'artist.gettoptracks',
-      api_key: this.lastFmApiKey,
-      format: 'json',
-      artist: artist.name,
-      limit
-    }
-    if (artist.mbid) params.mbid = artist.mbid;
+	async getArtistTopSongs(artist: Artist, limit = 10): Promise<LastFM.Track[]> {
+		const params: any = {
+			method: "artist.gettoptracks",
+			api_key: this.lastFmApiKey,
+			format: "json",
+			artist: artist.name,
+			limit,
+		};
+		if (artist.mbid) params.mbid = artist.mbid;
 
-    const topSongs = await axios.get('http://ws.audioscrobbler.com/2.0/', { params });
+		const topSongs = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+			params,
+		});
 
-    return topSongs.data.toptracks.track;
-  }
+		return topSongs.data.toptracks.track;
+	}
 
-  async getArtistAlbums(artist: Artist, limit = 10): Promise<LastFM.Album[]> {
-    const params: any = {
-      method: 'artist.gettopalbums',
-      api_key: this.lastFmApiKey,
-      format: 'json',
-      artist: artist.name,
-      limit
-    }
-    if (artist.mbid) params.mbid = artist.mbid;
+	async getArtistAlbums(artist: Artist, limit = 10): Promise<LastFM.Album[]> {
+		const params: any = {
+			method: "artist.gettopalbums",
+			api_key: this.lastFmApiKey,
+			format: "json",
+			artist: artist.name,
+			limit,
+		};
+		if (artist.mbid) params.mbid = artist.mbid;
 
-    const albums = await axios.get('http://ws.audioscrobbler.com/2.0/', { params });
+		const albums = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+			params,
+		});
 
-    return albums.data.topalbums.album;
-  }
+		return albums.data.topalbums.album;
+	}
 
-  async getAlbumInfo(album: Album): Promise<LastFM.Album>{
-    const params: any = {
-      method: 'album.getinfo',
-      api_key: this.lastFmApiKey,
-      format: 'json',
-      album: album.title,
-      artist: album.artistName
-    }
-    if (album.mbid) params.mbid = album.mbid;
+	async getAlbumInfo(album: Album): Promise<LastFM.Album> {
+		const params: any = {
+			method: "album.getinfo",
+			api_key: this.lastFmApiKey,
+			format: "json",
+			album: album.title,
+			artist: album.artistName,
+		};
+		if (album.mbid) params.mbid = album.mbid;
 
-    const albums = await axios.get('http://ws.audioscrobbler.com/2.0/', { params });
+		const albums = await axios.get("http://ws.audioscrobbler.com/2.0/", {
+			params,
+		});
 
-    return albums.data.album;
-  }
+		return albums.data.album;
+	}
 
+	removeCachedDuplicateSongs(cachedArray: Song[], array2: any[]) {
+		const cachedLinks = new Set(cachedArray.map((t) => t.lastFMLink));
+		const cachedMBIDs = new Set(cachedArray.map((t) => t.mbid));
+		const cachedKeys = new Set(
+			cachedArray.map(
+				(t) => `${t.title.toLowerCase()}:${t.artistName.toLowerCase()}`,
+			),
+		);
 
-  removeCachedDuplicateSongs(cachedArray: Song[], array2: any[]) {
-    const cachedLinks = new Set(cachedArray.map(t => t.lastFMLink));
-    const cachedMBIDs = new Set(cachedArray.map(t => t.mbid));
-    const cachedKeys = new Set(
-      cachedArray.map(t => `${t.title.toLowerCase()}:${t.artistName.toLowerCase()}`)
-    );
+		return array2.filter((externalTrack) => {
+			const key = `${externalTrack.name.toLowerCase()}:${(externalTrack.artist || externalTrack.artist?.name).toString()?.toLowerCase()}`;
+			return (
+				!cachedLinks.has(externalTrack.url) ||
+				!cachedMBIDs.has(externalTrack.mbid) ||
+				!cachedKeys.has(key)
+			);
+		});
+	}
 
-    return array2.filter((externalTrack) => {
-      const key = `${externalTrack.name.toLowerCase()}:${(externalTrack.artist || externalTrack.artist?.name).toString()?.toLowerCase()}`;
-      return !cachedLinks.has(externalTrack.url)
-        || !cachedMBIDs.has(externalTrack.mbid)
-        || !cachedKeys.has(key);
-    });
-  }
+	removeCachedDuplicateArtists(cachedArray: Artist[], array2: any[]) {
+		const cachedLinks = new Set(cachedArray.map((t) => t.lastFMLink));
+		const cachedMBIDs = new Set(cachedArray.map((t) => t.mbid));
 
-  removeCachedDuplicateArtists(cachedArray: Artist[], array2: any[]) {
-    const cachedLinks = new Set(cachedArray.map(t => t.lastFMLink));
-    const cachedMBIDs = new Set(cachedArray.map(t => t.mbid));
+		return array2.filter((externalArtist) => {
+			return (
+				!cachedLinks.has(externalArtist.url) ||
+				!cachedMBIDs.has(externalArtist.mbid)
+			);
+		});
+	}
 
-    return array2.filter((externalArtist) => {
-      return !cachedLinks.has(externalArtist.url)
-        || !cachedMBIDs.has(externalArtist.mbid)
-    });
-  }
+	removeCachedDuplicateAlbums(cachedArray: Album[], array2: any[]) {
+		const cachedMBIDs = new Set(cachedArray.map((t) => t.mbid));
+		const cachedNames = new Set(cachedArray.map((t) => t.title));
 
-  removeCachedDuplicateAlbums(cachedArray: Album[], array2: any[]) {
-    const cachedMBIDs = new Set(cachedArray.map(t => t.mbid));
-    const cachedNames = new Set(cachedArray.map(t => t.title));
-
-    return array2.filter((externalArtist) => {
-      return !cachedMBIDs.has(externalArtist.mbid)
-        || !cachedNames.has(externalArtist.name);
-    });
-  }
-
+		return array2.filter((externalArtist) => {
+			return (
+				!cachedMBIDs.has(externalArtist.mbid) ||
+				!cachedNames.has(externalArtist.name)
+			);
+		});
+	}
 }
